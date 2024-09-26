7318d2529db2615e53d847fbb31a05936866f66a
Ben Cabot <bencabot@microsoft.com>
2024-09-26 11:09:35 -0700

    Moved some files around in an attempt to get env variables working.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Load environment variables from .env file
 // require('dotenv').config();
 
+
 document.getElementById('send-button').addEventListener('click', sendMessage);
 
 function sendMessage() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
+// webpack.config.js
+const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  // other configurations...
+  mode: 'production', // or 'production' based on your needs
+  entry: './src/index.js', // Adjust the entry point as needed
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   plugins: [
     new Dotenv()
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 };
